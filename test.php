df90c4d187e3159989ff43150e7c06184a78c520
Eddie Jibson <jibson@tuta.io>
2019-05-22 16:42:43 +0100

    first

---
+++ test.php
@@ -0,0 +1,22 @@
+<?php
+
+require "vendor/autoload.php"; //Require composer's autoload
+
+//Custom options can be set. All are optional.
+$options = [
+    "report" => dirname(__FILE__) . "/reports/" . "report.xml", //Report file name. Set to false to disable generation fully.
+];
+
+//Intialize the Sido class and pass the options defined into it.
+//The options array is not required and you may pass in nothing.
+$sido = new \eddiejibson\Sido($options);
+
+//Set the test you're currently running
+$sido->setTest("Array test");
+
+//Test array we will be using
+$array = ["hello" => true];
+
+//Add testcases to the test
+$sido->should(is_array($array), "Be an array");
+$sido->should(count($array) > 1, "Have a length greater than 0");
